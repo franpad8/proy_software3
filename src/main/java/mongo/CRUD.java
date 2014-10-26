89d174d0c6a9ec284f4c6cdac201cbb2ebdaa9e4
JoseJulian <josejulianprado@gmail.com>
2014-10-26 15:13:35 -0430

    Agregado chequeos

--- src/main/java/mongo/CRUD.java
+++ src/main/java/mongo/CRUD.java
@@ -1,4 +1,3 @@
-
 package mongo;
 
 import com.mongodb.BasicDBObject;
@@ -27,8 +26,8 @@ public class CRUD {
     public CRUD(String databaseName, String databaseServer) throws IOException {
         this.databaseName = databaseName;
         this.databaseServer = databaseServer;
-        MongoClient client = 
-                new MongoClient(new ServerAddress(this.databaseServer, 27017));
+        MongoClient client
+                = new MongoClient(new ServerAddress(this.databaseServer, 27017));
         database = client.getDB(databaseName);
 
     }
@@ -44,30 +43,28 @@ public class CRUD {
         DBObject crit = new BasicDBObject("_id", id);
         return coll.findOne(crit);
     }
-    
+
     public DBObject findById(String collection, Object id) {
         DBCollection coll = database.getCollection(collection);
         DBObject crit = new BasicDBObject("_id", id);
         return coll.findOne(crit);
     }
-    
+
     public void updateObject(String collection, DBObject value) {
         DBCollection coll = database.getCollection(collection);
         coll.save(value);
     }
-    
+
     public void insertObject(String collection, DBObject value) {
         DBCollection coll = database.getCollection(collection);
         coll.insert(value);
     }
-    
+
     public void removeObject(String collection, DBObject value) {
         DBCollection coll = database.getCollection(collection);
         coll.remove(value);
     }
-    
-    
-    
+
     public String getDatabaseName() {
         return databaseName;
     }
@@ -91,48 +88,56 @@ public class CRUD {
     public void setDatabase(DB database) {
         this.database = database;
     }
-    
-    public void insertCollection(String collection, String nombre , String requisito, String prioridad) {
+
+    public void insertCollection(String collection, String nombre, String requisito, String prioridad) {
         //Esta es la coleccion proyectos
         DBCollection coll = database.getCollection(collection);
-        
-        requisito = requisito.toLowerCase();
-        prioridad = prioridad.toLowerCase();
+
+        requisito = capitalize(requisito.trim());
+        prioridad = capitalize(prioridad.trim());
         //Borrar ocurrencia si existe
         BasicDBObject temp = new BasicDBObject().append("nombre", requisito);
-        BasicDBObject aux = new BasicDBObject().append("$pull", new BasicDBObject("requisitos",temp));
+        BasicDBObject aux = new BasicDBObject().append("$pull", new BasicDBObject("requisitos", temp));
         BasicDBObject search = new BasicDBObject().append("nombre", nombre);
         coll.update(search, aux);
 
-        coll= database.getCollection(collection);
-        
-        
+        coll = database.getCollection(collection);
+
         //proyecto a modificar
-        BasicDBObject elemento = new BasicDBObject().append("nombre", requisito).append("prioridad",prioridad);
-        
+        BasicDBObject elemento = new BasicDBObject().append("nombre", requisito).append("prioridad", prioridad);
+
         BasicDBObject newDocument = new BasicDBObject();
-	newDocument.append("$addToSet", new BasicDBObject("requisitos",elemento));
+        newDocument.append("$addToSet", new BasicDBObject("requisitos", elemento));
         System.out.println(elemento.toString());
         System.out.println(newDocument.toString());
 //        newDocument.append("$addToSet", newDocument.append("prioridad", prioridad));
-	BasicDBObject searchQuery = new BasicDBObject().append("nombre", nombre);
-	coll.update(searchQuery, newDocument);
+        BasicDBObject searchQuery = new BasicDBObject().append("nombre", nombre);
+        coll.update(searchQuery, newDocument);
     }
-    
-     public void insertCollectionCeremonia(String collection, String id,  String usuario , String tipo, String reporte, String fecha) {
+
+    public void insertCollectionCeremonia(String collection, String id, String usuario, String tipo, String reporte, String fecha) {
         //Esta es la coleccion proyectos
         DBCollection coll = database.getCollection(collection);
         //proyecto a modificar
-        BasicDBObject elemento = new BasicDBObject().append("tipo", tipo).append("usuario", usuario).append("reporte",reporte).append("fecha", fecha);
-        
+        BasicDBObject elemento = new BasicDBObject().append("tipo", tipo).append("usuario", usuario).append("reporte", reporte).append("fecha", fecha);
+
         BasicDBObject newDocument = new BasicDBObject();
-	newDocument.append("$addToSet", new BasicDBObject("ceremonias",elemento));
+        newDocument.append("$addToSet", new BasicDBObject("ceremonias", elemento));
         System.out.println(elemento.toString());
         System.out.println(newDocument.toString());
 //        newDocument.append("$addToSet", newDocument.append("prioridad", prioridad));
-        ObjectId crit = (ObjectId)JSON.parse(id);
-	BasicDBObject searchQuery = new BasicDBObject().append("_id", crit);
-	coll.update(searchQuery, newDocument);
+        ObjectId crit = (ObjectId) JSON.parse(id);
+        BasicDBObject searchQuery = new BasicDBObject().append("_id", crit);
+        coll.update(searchQuery, newDocument);
     }
-    
+
+    private static String capitalize(String s) {
+        if (s.length() == 0) {
+            return s;
+        }
+        char first = s.charAt(0);
+        char capitalized = Character.toUpperCase(first);
+        return (first == capitalized)? s : capitalized + s.substring(1);
+    }
+
 }
